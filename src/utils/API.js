bcb9ace5f211145cf1ba208c7a9826bc547dc739
Annema Cozmoz <mikewittorp@gmail.com>
2021-02-04 16:37:22 -0500

    Deleting unnecessary details

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,14 +1,7 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/?results=1000&nat=us";
 const NewsURL =
-  "https://newsapi.org/v2/everything?q=broadway&pageSize=50&apiKey=c20dd1de26954654be5226fb76eaa7f9";
-
-// export default {
-//   getUsers: function () {
-//     return axios.get(BASEURL);
-//   },
-// };
+  "https://newsapi.org/v2/everything?q=broadway&pageSize=100&apiKey=c20dd1de26954654be5226fb76eaa7f9";
 
 export default {
   getUsers: function () {
